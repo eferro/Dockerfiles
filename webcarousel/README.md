4f78cff52e2ba74deec16021a7f8b13046d419ca
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-05-19 19:42:55 +0200

    Use a env var to pass the urls

---
+++ webcarousel/README.md
@@ -0,0 +1,1 @@
+docker run --name webcarousel -p 8888:8888 -e URLS=http://www.alea-soluciones.com/,http://www.microsoft.com -d aleasoluciones/webcarousel
--- webcarousel/launcher.py
+++ webcarousel/launcher.py
@@ -1,9 +1,11 @@
 import tornado.ioloop
 import tornado.web
+import os
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-    	self.render('index.html')
+        urls = os.environ['URLS'].split(',')
+        self.render('index.html', urls=urls, initial_url=urls[0])
 
 application = tornado.web.Application([
     (r"/", MainHandler),
